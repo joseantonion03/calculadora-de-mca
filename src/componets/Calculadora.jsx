f4c1f65a98dc5ae930b58a1bf992ad54bbadd566
José Antônio <50558024+joseantonion03@users.noreply.github.com>
2023-09-29 15:34:18 -0300

    Correção no texto da tag OPTION

--- src/componets/Calculadora.jsx
+++ src/componets/Calculadora.jsx
@@ -103,7 +103,7 @@ const Calculadora = () => {
             className="bg-gray-50 py-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option value="" disabled>
-              Selecione a vazão
+              Selecione a tubulação (Milímetro)
             </option>
             <option value="20">20</option>
             <option value="25">25</option>

c185cfbeb17938f4e241f8ad1364423a8c0e54a6
José Antônio <50558024+joseantonion03@users.noreply.github.com>
2024-02-12 10:54:16 -0300

    Add files via upload

--- src/componets/Calculadora.jsx
+++ src/componets/Calculadora.jsx
@@ -225,7 +225,7 @@ const Calculadora = () => {
         >
           Calcular
         </button>
-        <a target="_blank" className="text-center mx-auto mt-3.5" href="https://www.joseantonion.com.br/">Desenvolvido por José Antônio</a>
+        <a target="_blank" className="text-center mx-auto mt-3.5" href="https://www.joseantonion.com.br/">Desenvolvido por José Antônio</a>        
       </form>
       <SpeedDial url="https://dimensionamento.vercel.app" />
       {loading && <LoadingPage texto="Buscando informações" />}
